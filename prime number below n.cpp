83517a050152a8d3ff5a797b9d5b2d40cdf7297c
Kaustubh Daruri <77714586+Kaustubh152003@users.noreply.github.com>
2023-05-04 12:55:27 +0530

    Update prime number below n.cpp

--- prime number below n.cpp
+++ prime number below n.cpp
@@ -54,6 +54,24 @@ int prime[100000];
     }
 
 
+int* binary_search(int* arr, int n,int k) // returns the adress where the element is found. If not found, returns NULL
+{
+    if(n==0)
+    return 0;
+    else
+    {
+        if(arr[n/2]==k)
+        return arr+n/2;
+        else
+        {
+            if(k<arr[n/2])
+            return binary_search(arr,n/2,k);
+            else
+            return binary_search(arr+n/2+1,n-n/2-1,k);
+        }
+    }
+}
+
 void prime_factorize(int x,int* table,int sp) //sets the prime factorization into the array "table". needs primes and table to be initialized
 {
     int count=0;
