2a7f656a9da040e1de4a7c20d412256a4a8c90a3
Kaustubh Daruri <77714586+Kaustubh152003@users.noreply.github.com>
2023-08-08 20:27:54 +0530

    Update merge_sort with your own compare function.cpp

--- merge_sort with your own compare function.cpp
+++ merge_sort with your own compare function.cpp
@@ -1,60 +1,60 @@
 int compare(int a,int b) //return 1 if a<b else 0 for ur comparating operation
+{
+    if(a<b)
+    return 1;
+    else
+    return 0;
+}
+void merge(int arr[], int l, int m, int r)
+{
+    int p=l;
+    int q=m+1;
+    int res[r-l+1];
+    int j=0;
+    while(p<=m && q<=r)
     {
-        if(a<b)
-        return 1;
-        else
-        return 0;
-    }
-    void merge(int arr[], int l, int m, int r)
-    {
-        int p=l;
-        int q=m+1;
-        int res[r-l+1];
-        int j=0;
-        while(p<=m && q<=r)
-        {
-            if(compare(arr[p],arr[q]))
-            {
-                res[j]=arr[p];
-                p++;
-                j++;
-            }
-            else
-            {
-                res[j]=arr[q];
-                q++;
-                j++;
-            }
-        }
-        while(p<=m)
+        if(compare(arr[p],arr[q]))
         {
             res[j]=arr[p];
             p++;
             j++;
         }
-        while(q<=r)
+        else
         {
             res[j]=arr[q];
             q++;
             j++;
         }
-        int k=l;
-        j=0;
-        while(k<=r)
-        {
-            arr[k]=res[j];
-            j++;
-            k++;
-        }
     }
-    void mergeSort(int arr[], int l, int r)
+    while(p<=m)
     {
-        if(l<r)
-        {
-            int m=(l+r)/2;
-            mergeSort(arr,l,m);
-            mergeSort(arr,m+1,r);
-            merge(arr,l,m,r);
-        }
-        
+        res[j]=arr[p];
+        p++;
+        j++;
     }
+    while(q<=r)
+    {
+        res[j]=arr[q];
+        q++;
+        j++;
+    }
+    int k=l;
+    j=0;
+    while(k<=r)
+    {
+        arr[k]=res[j];
+        j++;
+        k++;
+    }
+}
+void mergeSort(int arr[], int l, int r)
+{
+    if(l<r)
+    {
+        int m=(l+r)/2;
+        mergeSort(arr,l,m);
+        mergeSort(arr,m+1,r);
+        merge(arr,l,m,r);
+    }
+    
+}
