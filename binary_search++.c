a5547963cb6e29fca67089ef5fc96b0c7643c7f1
Kaustubh Daruri <77714586+Kaustubh152003@users.noreply.github.com>
2024-02-27 22:20:09 +0530

    Update binary_search++.c

--- binary_search++.c
+++ binary_search++.c
@@ -15,6 +15,24 @@ int* binary_search(int* arr, int n,int k) // returns the adress where the elemen
         }
     }
 }
+int bs(int* arr,int l,int r,int k) 
+{
+    if(l<=r)
+    {
+        int m=(l+r)/2;
+        if(k==arr[m])
+        return m;
+        else
+        {
+            if(k<arr[m])
+            return bsl(arr,l,m-1,k);
+            else
+            return bsl(arr,m+1,r,k);
+        }
+    }
+    else
+    return -1;
+}
 int bsl(int* arr,int l,int r,int k) //binary search which returns the left most occurence of element k
 {
     if(l<=r)
